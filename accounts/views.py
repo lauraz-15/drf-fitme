f7e94fe08b97132b73c9349b899eabe7e479683e
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-11 13:12:37 +0000

    Add accounts view and link urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Account
 
-# Create your views here.
+
+class AccountList(APIView):
+    """
+    Display a list of all accounts
+    """
+    def get(self, request):
+        accounts = Account().objects.all()
+        return Response(accounts)
+        
