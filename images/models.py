4b2b45c1afa28f11a78daab6686f5cfdb6180d70
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-22 14:27:28 +0000

    Add images app and create Image model

---
+++ images/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Image(models.Model):
+    """
+    Image model, related to 'owner', i.e. a User instance.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True)
+    picture = models.ImageField(
+        upload_to='images/', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f'{self.id}'
