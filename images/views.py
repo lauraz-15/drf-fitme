73778c4238c3c7e488d3087cac047725230f0983
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-23 08:06:36 +0000

    Add ImageList view API

--- images/views.py
+++ images/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Image
+from .serializers import ImageSerializer
 
-# Create your views here.
+class ImageList(APIView):
+    def get(self, request):
+        images = Image.objects.all()
+        serializer = ImageSerializer(
+            images, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+        
