ad742d15c583523f041b4f53ad0a13384209cf50
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-23 07:57:10 +0000

    Add photo file validation method

--- images/serializers.py
+++ images/serializers.py
@@ -8,6 +8,21 @@ class ImageSerializer(serializers.ModelSerializer):
     account_id = serializers.ReadOnlyField(source='owner.account.id')
     account_image = serializers.ReadOnlyField(source='owner.account.image.url')
 
+    def validate_picture(self, value):
+        if value.size > 1024 * 1024 * 3:
+            raise serializers.ValidationError(
+                'The file size is larger 3 MB!'
+            )
+        if value.picture.width > 4000:
+            raise serializers.ValidationError(
+                "The width can't exceed 4000px!"
+            )
+        if value.picture.height > 4000:
+            raise serializers.ValidationError(
+                "The height can't exceed 4000px!"
+            )
+        return value
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
