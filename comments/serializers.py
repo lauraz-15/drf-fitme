dfd668a44c21cb8a7e74699f464130e58b0796a8
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-25 14:36:30 +0000

    Add CommentDetailSerializer

--- comments/serializers.py
+++ comments/serializers.py
@@ -22,3 +22,11 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'account_id',
             'account_image', 'image', 'created_on', 'updated_on', 'text',
         ]
+
+
+class CommentDetailSerializer(CommentSerializer):
+    """
+    Detail view Comment serializer
+    Image is a read only field so that we dont have to get it repeatedly
+    """
+    image = serializers.ReadOnlyField(source='image.id')
