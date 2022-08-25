c507626efddbfd78bc1c9521771b0d2cad15db5b
lauraz-15 <laura.zvirbule90@gmail.com>
2022-08-25 09:58:44 +0000

    Add delete method to ImgaeDetail view

--- images/views.py
+++ images/views.py
@@ -65,3 +65,10 @@ class ImageDetail(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        image = self.get_object(pk)
+        image.delete()
+        return Response(
+            status=status.HTTP_404_NOT_FOUND
+        )
