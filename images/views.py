e84712b00de88f123797b39e8abbd923202e6d89
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-08 20:01:51 +0000

    Refactor Image views using generics

--- images/views.py
+++ images/views.py
@@ -1,74 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import generics, permissions
 from .models import Image
 from .serializers import ImageSerializer
 from main.permissions import isOwnerOrViewOnly
 
-class ImageList(APIView):
+class ImageList(generics.ListAPIView):
     """
     Display a list of all images
+    Add a new image if logged in
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Image.objects.all()
     serializer_class = ImageSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly]
 
-    def get(self, request):
-        images = Image.objects.all()
-        serializer = ImageSerializer(
-            images, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
-    
-    def post(self, request):
-
-        serializer = ImageSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
-class ImageDetail(APIView):
+class ImageDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Display an individual image and account details
+    Display an individual image
+    Update and Delete option visible for image owners
     """
+    permission_classes = [isOwnerOrViewOnly]
+    queryset = Image.objects.all()
     serializer_class = ImageSerializer
-    permission_classes  = [isOwnerOrViewOnly]
-    def get_object(self, pk):
-        try:
-            image = Image.objects.get(pk=pk)
-            self.check_object_permissions(self.request, image)
-            return image
-        except Image.DoesNotExist:
-            raise Http404
-    
-    def get(self, request, pk):
-        image = self.get_object(pk)
-        serializer = ImageSerializer(
-            image, context={'request': request}
-            )
-        return Response(serializer.data)
-    
-
-    def put(self, request, pk):
-        image = self.get_object(pk)
-        serializer = ImageSerializer(
-            image, context={'request': request}, data=request.data
-            )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        image = self.get_object(pk)
-        image.delete()
-        return Response(
-            status=status.HTTP_404_NOT_FOUND
-        )
