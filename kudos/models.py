d2dac372f243a9eb0d54c7664a6857716032b55f
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-08 13:22:53 +0000

    Add Kudos app and Kudos model class

---
+++ kudos/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from images.models import Image
+
+
+class Kudos(models.Model):
+    """
+    Kudos model, related to User and Image.
+    'unique_together' ensures that one user can't add kudos 
+    to the same image twice.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(
+        Image, on_delete=models.CASCADE, related_name='kudos')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+        unique_together = ['owner', 'image']
+
+    def __str__(self):
+        return f'{self.owner} {self.image}'
--- main/settings.py
+++ main/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'accounts',
     'images',
     'comments',
+    'kudos',
 ]
 
 MIDDLEWARE = [
