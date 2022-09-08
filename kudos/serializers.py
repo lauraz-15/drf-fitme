a01276098a727ded24fcf00037a4f7d74ad766c1
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-08 13:43:21 +0000

    Add create function to kudos serializer to handle duplicates

---
+++ kudos/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Kudos
+from django.db import IntegrityError
+
+class KudosSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Kudos model
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+
+    class Meta:
+        model = Kudos
+        fields = [
+            'id', 'owner', 'created_on','image',
+        ]
+    
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'duplicate kudos'
+            })
