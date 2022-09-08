1a186cc1ceaf0f57f11ab21aaa7afca67f306e0c
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-08 12:38:40 +0000

    Add Comment Deatail view(retrieve, update, delete)

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Image
+from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
--- comments/views.py
+++ comments/views.py
@@ -4,12 +4,15 @@ from .models import Comment
 from .serializers import CommentDetailSerializer, CommentSerializer
 
 
-
-class CommentList(generics.ListAPIView):
+class CommentList(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
-        
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [isOwnerOrViewOnly]
+    queryset = Comment.objects.all()
+    serializer_class = CommentDetailSerializer()
