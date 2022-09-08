86d985de3c053b1648f16e4b750c3306d61d8a2b
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-08 15:57:42 +0000

    Add FollowerList genereic view

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from main.permissions import isOwnerOrViewOnly
+from .models import Follower
+from .serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    Display a list of all followed users.
+    Add new follower, if user logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
