e66aebeb920cd61180729146d651b746775ef9be
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-09 09:18:22 +0000

    Add search field to search by image description or username

--- images/views.py
+++ images/views.py
@@ -15,11 +15,16 @@ class ImageList(generics.ListAPIView):
         kudos_count=Count('kudos', distinct=True),
     ).order_by('-created_on')
     serializer_class = ImageSerializer
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [filters.OrderingFilter,
+    filters.SearchFilter]
     ordering_fields = [
         'comments_count',
         'kudos_count',
         'kudos__created_on',
+    ]
+    search_fields = [
+        'owner__username',
+        'description',
     ]
 
     def perform_create(self, serializer):
@@ -32,7 +37,7 @@ class ImageDetail(generics.RetrieveUpdateDestroyAPIView):
     Update and Delete option visible for image owners
     """
     permission_classes = [isOwnerOrViewOnly]
-    queryset = Image.objects.annotate(
+    Listqueryset = Image.objects.annotate(
         comments_count=Count('comment', distinct=True),
         kudos_count=Count('kudos', distinct=True),
     ).order_by('-created_on')
