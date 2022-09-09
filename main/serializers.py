13ce243c2460a2c2a84c3dbd6b9a8e66f616268b
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-09 10:50:57 +0000

    Add settings on main serializers.py file to get user image and username for JWT

---
+++ main/serializers.py
@@ -0,0 +1,9 @@
+from dj_rest_auth.serializers import UserDetailsSerializer
+from rest_framework import serializers
+
+
+class LoggedInUserSerializer(UserDetailsSerializer):
+    account_id = serializers.ReadOnlyField(source='account.id')
+    account_image = serializers.ReadOnlyField(source='account.image.url')
+    class Meta(UserDetailsSerializer.Meta):
+        fields = UserDetailsSerializer.Meta.fields + ('account_id', 'account_image')
