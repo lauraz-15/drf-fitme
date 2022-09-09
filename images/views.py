36203c06a012ffaf1b3d2294460dbf4f399154f2
lauraz-15 <laura.zvirbule90@gmail.com>
2022-09-09 10:04:41 +0000

    Add filterset fields and fix the Image create option

--- images/views.py
+++ images/views.py
@@ -1,22 +1,28 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Image
 from .serializers import ImageSerializer
 from main.permissions import isOwnerOrViewOnly
 
-class ImageList(generics.ListAPIView):
+
+class ImageList(generics.ListCreateAPIView):
     """
     Display a list of all images
     Add a new image if logged in
     """
+    serializer_class = ImageSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Image.objects.annotate(
         comments_count=Count('comment', distinct=True),
         kudos_count=Count('kudos', distinct=True),
     ).order_by('-created_on')
-    serializer_class = ImageSerializer
-    filter_backends = [filters.OrderingFilter,
-    filters.SearchFilter]
+
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        ]
     ordering_fields = [
         'comments_count',
         'kudos_count',
@@ -25,6 +31,11 @@ class ImageList(generics.ListAPIView):
     search_fields = [
         'owner__username',
         'description',
+    ]
+    filterset_fields = [
+        'owner__followed__owner__account',
+        'kudos__owner__account',
+        'owner__account',
     ]
 
     def perform_create(self, serializer):
@@ -37,7 +48,7 @@ class ImageDetail(generics.RetrieveUpdateDestroyAPIView):
     Update and Delete option visible for image owners
     """
     permission_classes = [isOwnerOrViewOnly]
-    Listqueryset = Image.objects.annotate(
+    queryset = Image.objects.annotate(
         comments_count=Count('comment', distinct=True),
         kudos_count=Count('kudos', distinct=True),
     ).order_by('-created_on')
